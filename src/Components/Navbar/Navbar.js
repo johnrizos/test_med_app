9933069e0138ef66faf6233d112148e78d7212ea
johnrizos <giannisrizos87@gmail.com>
2024-08-15 18:27:03 -0400

    fix the conection with server for coursera mongodb

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,13 +1,35 @@
-// react function add Navbar.html inside return
-// add in Navbar.js links for Login.js and Sign_Up.js
-
-import React from "react";
+import React,{ useEffect, useState } from "react";
 import "./Navbar.css";
 import { Link } from 'react-router-dom';
 
 
-
 function Navbar() {
+
+  const [isLoggedIn,setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const[email,setEmail]=useState("");
+
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("auth-token");
+    sessionStorage.removeItem("name");
+    sessionStorage.removeItem("email");
+    sessionStorage.removeItem("phone");
+    // remove email phone
+    localStorage.removeItem("doctorData");
+    setIsLoggedIn(false);
+    setEmail('');
+    window.location.reload();
+}
+
+useEffect(() => { 
+  const storedemail = sessionStorage.getItem("email");
+
+  if (storedemail) {
+        setIsLoggedIn(true);
+        setUsername(storedemail.split("@")[0]);
+      }
+    }, []);
   return (
     <div>
       <nav className="navbar">
@@ -17,25 +39,30 @@ function Navbar() {
         <ul className="nav-links">
           <li>
 
-            <a href="#home">Home</a>
+          <Link to="/">Home</Link>
           </li>
           <li>
-            <a href="#appointment">Appointment</a>
+          <Link to="/search/doctors">Appointments</Link>
 
           </li>
           <li>
-            <a href="#reviews">Reviews</a>
+          <Link to="/reviews">Reviews</Link>
           </li>
-          <li>
-            <Link to="signup">
-              <button>Sign Up</button>
-            </Link>
-          </li>
-          <li>
-            <Link to="login">
-              <button>Login</button>
-            </Link>
-          </li>
+          {isLoggedIn ? ( // Conditional rendering
+            <><li>
+              <Link to="/profile">Welcome, {username}</Link>
+            </li>
+            <li>
+                <button onClick={handleLogout}>Logout</button>
+            </li></>
+          ) : ( // Else part of conditional rendering   
+            <>
+              <li><Link to="/signup"><button>Sign Up</button></Link></li>
+              <li><Link to="/login"><button>Login</button></Link></li>
+            </>
+          )}
+          
+
         </ul>
       </nav>
     </div>
